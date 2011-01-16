4e1909707c1f175aff5830860c8dcf6ced10312c
Stephen Touset <stephen@touset.org>
2011-01-16 15:00:22 -0500

    Make testing autotest-compatible

--- Rakefile
+++ Rakefile
@@ -2,6 +2,6 @@ require 'rake/testtask'
 
 Rake::TestTask.new do |test|
   test.libs   << 'test'
-  test.pattern = 'test/*_test.rb'
+  test.pattern = 'test/**/test_*.rb'
   test.verbose = true
 end
--- test/unit/test_transloadit.rb
+++ test/unit/test_transloadit.rb
@@ -12,11 +12,17 @@ class TestTransloadit < MiniTest::Unit::TestCase
   
   def test_initializer_requires_key
     assert_raises ArgumentError do
-      t = Transloadit.new(:secret => 'x')
+      Transloadit.new(:secret => 'x')
     end
   end
   
   def test_initializer_does_not_require_secret
     assert_kind_of Transloadit, Transloadit.new(:key => 'x')
+  end
+  
+  def test_initializer_with_no_arguments
+    assert_raises ArgumentError do
+      Transloadit.new
+    end
   end
 end
