7a436a87a4655338680d749a62b5029d0df697e7
Robin Mehner <robin@coding-robin.de>
2013-04-17 14:16:06 +0200

    Remove trailing whitespaces

--- lib/transloadit/response/assembly.rb
+++ lib/transloadit/response/assembly.rb
@@ -4,11 +4,11 @@ module Transloadit::Response::Assembly
   def reload!
     self.replace Transloadit::Request.new(self['assembly_url']).get
   end
-  
+
   def cancel!
     self.replace Transloadit::Request.new(self['assembly_url']).delete
   end
-  
+
   def completed?
     self['ok'] == 'ASSEMBLY_COMPLETED'
   end
--- test/unit/transloadit/test_response.rb
+++ test/unit/transloadit/test_response.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 describe Transloadit::Response do
   REQUEST_URI = 'http://api2.jane.transloadit.com/assemblies/76fe5df1c93a0a530f3e583805cf98b4'
-  
+
   it 'must allow delegate initialization' do
     response = Transloadit::Response.new('test')
     response.class.must_equal Transloadit::Response
   end
-  
+
   describe 'when initialized' do
     before do
       VCR.use_cassette 'fetch_assembly' do
@@ -16,11 +16,11 @@ describe Transloadit::Response do
         )
       end
     end
-    
+
     it 'must parse the body' do
       @response.body.must_be_kind_of Hash
     end
-    
+
     it 'must allow access to body attributes' do
       %w{ ok message assembly_id assembly_url }.each do |attribute|
         @response[attribute].must_equal @response.body[attribute]
@@ -32,12 +32,12 @@ describe Transloadit::Response do
         @response[attribute].must_equal @response.body[attribute.to_s]
       end
     end
-    
+
     it 'must inspect as the body' do
       @response.inspect.must_equal @response.body.inspect
     end
   end
-  
+
   describe 'when extended as an assembly' do
     before do
       VCR.use_cassette 'fetch_assembly' do
@@ -46,26 +46,26 @@ describe Transloadit::Response do
         ).extend!(Transloadit::Response::Assembly)
       end
     end
-    
+
     it 'must allow checking for completion' do
       @response.completed?.must_equal true
     end
-    
+
     # TODO: can this be tested better?
     it 'must allow reloading the assembly' do
       VCR.use_cassette 'fetch_assembly', :allow_playback_repeats => true do
         @response.send(:__getobj__).
           wont_be_same_as @response.reload!.send(:__getobj__)
-        
+
         @response.object_id.
           must_equal @response.reload!.object_id
       end
     end
-    
+
     it 'must allow canceling' do
       VCR.use_cassette 'cancel_assembly' do
         @response.cancel!
-        
+
         @response.completed?.must_equal false
         @response['ok']     .must_equal 'ASSEMBLY_CANCELED'
       end
