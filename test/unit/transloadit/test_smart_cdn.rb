1023da60dd47f6d0b6514c9b4a630711594dccac
Kevin van Zonneveld <kevin@vanzonneveld.net>
2024-11-26 12:42:46 +0100

    Harcode urls

--- test/unit/transloadit/test_smart_cdn.rb
+++ test/unit/transloadit/test_smart_cdn.rb
@@ -47,7 +47,7 @@ describe Transloadit do
         input: "",
         expire_at_ms: @expire_at
       )
-      assert_match %r{^https://my-app\.tlcdn\.com/test-smart-cdn/\?auth_key=my-key&exp=1732550672867&sig=sha256%3A[a-f0-9]+$}, url
+      assert_equal "https://my-app.tlcdn.com/test-smart-cdn/?auth_key=my-key&exp=1732550672867&sig=sha256%3Ad5e13df4acde8d4aaa0f34534489e54098b5128c54392600ed96dd77669a533e", url
     end
 
     it "uses instance credentials by default" do
@@ -57,7 +57,7 @@ describe Transloadit do
         input: @input,
         expire_at_ms: @expire_at
       )
-      assert_match %r{^https://my-app\.tlcdn\.com/test-smart-cdn/inputs%2Fprinsengracht\.jpg\?auth_key=my-key&exp=1732550672867&sig=sha256%3A[a-f0-9]+$}, url
+      assert_equal "https://my-app.tlcdn.com/test-smart-cdn/inputs%2Fprinsengracht.jpg?auth_key=my-key&exp=1732550672867&sig=sha256%3A8620fc2a22aec6081cde730b7f3f29c0d8083f58a68f62739e642b3c03709139", url
     end
 
     it "allows overriding credentials" do
@@ -69,7 +69,7 @@ describe Transloadit do
         auth_key: "override-key",
         auth_secret: "override-secret"
       )
-      assert_match %r{^https://my-app\.tlcdn\.com/test-smart-cdn/inputs%2Fprinsengracht\.jpg\?auth_key=override-key&exp=1732550672867&sig=sha256%3A[a-f0-9]+$}, url
+      assert_equal "https://my-app.tlcdn.com/test-smart-cdn/inputs%2Fprinsengracht.jpg?auth_key=override-key&exp=1732550672867&sig=sha256%3A90734593f8d05e09afc6869d9e37339a85a7041974c133d4663c2376f2736983", url
     end
 
     it "includes empty width parameter" do
@@ -83,7 +83,7 @@ describe Transloadit do
           height: 200
         }
       )
-      assert_match %r{^https://my-app\.tlcdn\.com/test-smart-cdn/inputs%2Fprinsengracht\.jpg\?auth_key=my-key&exp=1732550672867&height=200&width=&sig=sha256%3A[a-f0-9]+$}, url
+      assert_equal "https://my-app.tlcdn.com/test-smart-cdn/inputs%2Fprinsengracht.jpg?auth_key=my-key&exp=1732550672867&height=200&width=&sig=sha256%3Aebf562722c504839db97165e657583f74192ac4ab580f1a0dd67d3d868b4ced3", url
     end
   end
 end
