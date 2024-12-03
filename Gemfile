93ec1aabed1d923779d305da7c3519149e319a76
Kevin van Zonneveld <kevin@vanzonneveld.net>
2024-12-03 12:53:45 +0100

    Coverage (#72)
    * Coverage
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update Gemfile
    
    * Update test_helper.rb
    
    * Format
    
    * Update README.md

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source "https://rubygems.org"
 
+gem "rake"
+gem "minitest"
+gem "simplecov"
+gem "simplecov-cobertura"
+
 gemspec
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,13 @@ $:.unshift File.expand_path("../../lib", __FILE__)
 
 if ENV["COVERAGE"] != "0"
   require "simplecov"
-  SimpleCov.start { add_filter "/test/" }
+  SimpleCov.start do
+    add_filter "/test/"
+    enable_coverage :branch
+  end
+
+  require "simplecov-cobertura"
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 
 require "minitest/autorun"
